47e3347402a448c31a6b4b5240699a035b3a8005
Janine Hempy <jhempy@gmail.com>
2017-10-31 15:46:04 -0400

    Prepare for database

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,19 @@
 */
 
 Route::get('/', function () {
-    $currentPlayer = 'Red';
+
+    $players = ['Red', 'Blue'];
+    $turn = 5;
+    $currentPlayer = $players[$turn % 2];
     $board = [];
     $rows = 6;
     $columns = 7;
     for ($r = 0; $r < $rows; $r++) {
       for ($c = 0; $c < $columns; $c++) {
-        $board[$r][$c] = 'red';
+        $board[$r][$c] = '';
       }
     }
+
     return view('board', compact('currentPlayer', 'board', 'rows', 'columns'));
+    
 });
