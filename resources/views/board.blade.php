58d675b12f3d0d3678986bf304beab33820605e1
Janine Hempy <jhempy@gmail.com>
2017-11-01 09:59:39 -0400

    Now, with arrows

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -4,19 +4,20 @@
   <title>Laravel Connect Four</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+  <script src="https://use.fontawesome.com/14f1f2c704.js"></script>
 </head>
 <body class="text-center container">
 
 <h1 class="mt-5">Laravel Connect Four</h1>
 
 <div class="row justify-content-center mt-5">
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
-  <div class="drop"><button class="btn btn-light">Drop!</button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
+  <div class="drop"><button class="btn btn-light"><i class="fa fa-arrow-down" aria-hidden="true"></i></button></div>
 </div>
 
 <div class="mt-2 mb-3 board">
